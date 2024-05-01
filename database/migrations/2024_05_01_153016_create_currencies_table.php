09f22af020541e18b0d92c571c06aad59c4c1ef5
TJChiang <tjchiang8419@gmail.com>
2024-05-01 16:41:24 +0800

    建立 currency data

--- database/migrations/2024_05_01_153016_create_currencies_table.php
+++ database/migrations/2024_05_01_153016_create_currencies_table.php
@@ -15,7 +15,6 @@ return new class extends Migration
             $table->id()->primary();
             $table->string('code',5)->unique();
             $table->string('name', 100);
-            $table->string('country', 100);
             $table->string('number', 5);
         });
     }
