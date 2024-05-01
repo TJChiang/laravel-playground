283667364d0eaadb236ffcc4df8253d7792f17b9
TJChiang <tjchiang8419@gmail.com>
2024-05-01 16:18:02 +0800

    建立 table

---
+++ database/migrations/2024_05_01_153016_create_currencies_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('currencies', function (Blueprint $table) {
+            $table->id()->primary();
+            $table->string('code',5)->unique();
+            $table->string('name', 100);
+            $table->string('country', 100);
+            $table->string('number', 5);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('currencies');
+    }
+};
